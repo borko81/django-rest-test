f1455b87d479941d8e35bb0eecf1e04e07dc420b
Borko <bstoilov81@gmail.com>
2020-12-18 13:54:44 +0200

    Add doc to test case

---
+++ workwithrest/company/comany_tests/test.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+from company.models import Company
+
+
+class MyTestClass(TestCase):
+    def setUp(self):
+        Company.objects.create(name='UnrealSoft', description='Some Desc', website='https://somewebsite.com', city='Velingrad')
+
+    def test_count_of_company(self):
+        self.assertEqual(Company.objects.all().count(), 1)
+
+    def test_first_param(self):
+        '''Testing name for first param'''
+        name = Company.objects.get(id=1)
+        self.assertEqual(name.name, 'UnrealSoft')
+
